038bacd672c73270d6e9bae0810f14e50c4b5501
sbx_user1051 <sbx_user1051@169.254.6.101>
2025-04-08 17:45:18 +0000

    Tue Apr 08 17:45:18 UTC 2025

--- test/solutions/HLO/test-hlo-solution.test.js
+++ test/solutions/HLO/test-hlo-solution.test.js
@@ -1,8 +1,9 @@
 const HelloSolution = require('../../../lib/solutions/HLO/hello_solution');
 
-describe('HelloSolution output Hello, World!', () => {
+describe('HelloSolution outputs expected string', () => {
     it('should return a string', () => {
         const helloSolution = new HelloSolution();
-        expect(helloSolution.hello()).toBe('Hello, World!');
+        expect(helloSolution.hello('John')).toBe('Hello, John!');
     });
 });
+
